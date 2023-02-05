209cd584265a1b1a64d6f24b8e1713196fa2dcbd
dworakowskam <dworakowska.m@o2.pl>
2023-02-05 20:55:58 +0100

    Final

--- blog/tests.py
+++ blog/tests.py
@@ -7,7 +7,7 @@ driver.get(url='http://127.0.0.1:8000/admin')
 
 def element_is_clickable():
     driver.find_element_by_xpath('//*[@id="id_username"]').send_keys("md")
-    driver.find_element_by_xpath('//*[@id="id_password"]').send_keys("adenina6")
+    driver.find_element_by_xpath('//*[@id="id_password"]').send_keys("kupa")
     time.sleep(2)
     driver.find_element_by_xpath('/html/body/div/div[2]/div/div[1]/div/form/div[3]/input').click()
     time.sleep(2)
